38f46fc36e01e4db6bf44c10a0fdb66296aadafa
Jonas Windhager <jonas.windhager@uzh.ch>
2021-01-19 01:15:49 +0100

    added test stubs

---
+++ napari_imc/_tests/test_imc.py
@@ -0,0 +1,22 @@
+def test_napari_get_reader():
+    pass  # TODO
+
+
+def test_read_imc():
+    pass  # TODO
+
+
+def test_read_mcd():
+    pass  # TODO
+
+
+def test_read_txt():
+    pass  # TODO
+
+
+def test_load_panorama():
+    pass  # TODO
+
+
+def test_load_acquisition():
+    pass  # TODO
